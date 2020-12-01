343e15ab9a824b103790ea65e9870806e89100a7
Falconno7 <xogusxogus39@naver.com>
2020-12-01 12:07:31 +0900

    start_condition

--- cordingTest풀이_파이썬/3과 5의 배수 문제 바킹덕/3과5의배수찾기.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-ans = 0
-for i in range(1, n):
-    if (i % 3) == 0 or (i % 5) == 0:
-        ans += i
-print(ans)
--- cordingTest풀이_파이썬/3과 5의 배수 문제 바킹덕/N이제곱수인지여부.py
+++
@@ -1,9 +0,0 @@
-def func(n):
-    num = int(n**(1/2))
-    if  num ** 2 == n:
-        print(1)
-    else :
-        print(0)
-func(9)
-func(693953651)
-func(756580036)
