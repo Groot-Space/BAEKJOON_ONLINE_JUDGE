47afa80a46e6bb961429611b1829322e69799782
Falconno7 <xogusxogus39@naver.com>
2020-12-31 00:10:10 +0900

    boj-2135

--- 태현/구현/백준 문제/2135_문자열 압축하기_백준/2135_solution.py
+++ 태현/구현/백준 문제/2135_문자열 압축하기_백준/2135_solution.py
@@ -1,33 +1,97 @@
-def makestring(l,s):
-    s = s[:l[0]] + str(l[1]) + l[2] + s[l[3]:]
-    return s
-def RLE(s_split,s):
+def choose(spl, s):
+    ret = []
     start = 0
-    end = s_split
+    end = spl
     while end <= len(s):
-        cri = s[start:end]
-        count = 1
-        com_start = end
-        com_end = end + s_split
-        while com_end <= len(s):
-            if cri == s[com_start:com_end]:
-                count += 1
-                com_start += s_split
-                com_end += s_split
-            elif count > 1 or (count > 1 and com_end <= len(s)):
-                s = makestring([start,count,cri,com_end-s_split],s)
-                break
-            else:
-                break
-        start += s_split
-        end += s_split
+        flag = 0
+        flag2 = 0
+        temp = s[start:end]
 
-    return len(s), s
+        for j in range(len(temp)-1):
+            if temp[j] != temp[j+1]:
+                flag2 += 1
+                break
+
+        if flag2 == 0 and spl != 1:
+            start += 1
+            end += 1
+            continue
+        if len(ret) != 0:
+
+            for i in ret:
+                if s[start:end] == i:
+                    flag += 1
+                    break
+        if flag == 0:
+            ret.append(s[start:end])
+        start += 1
+        end += 1
+        if end > len(s): break
+    return ret
+
+def RLE(ret, s):
+    result = []
+    for i in range(len(ret)):
+        st = ''
+        count = 0
+        start = 0
+        end = len(ret[i])
+
+        while end <= len(s):
+            if s[start:end] == ret[i]:
+                temp_start = start + len(ret[i])
+                temp_end = end + len(ret[i])
+                count += 1
+
+                while end <= len(s):
+                    if s[temp_start:temp_end] != ret[i]:
+                        break
+                    count += 1
+                    temp_start += len(ret[i])
+                    temp_end += len(ret[i])
+
+                if count > 1 :
+                    if len(ret[i])*count >= len(ret[i]) + 3:
+                        st = s[:start] + str(count) + '(' + ret[i] + ')' + s[temp_end-len(ret[i]):]
+                    break
+                count = 0
+            start += 1
+            end += 1
+
+        if len(st) != 0:
+            result.append(st)
+
+
+    if len(result) != 0:
+        s_list = []
+        m = len(result[0])
+        for i in range(len(result)):
+            if len(result[i]) <= m:
+                s_list.append(result[i])
+                m = len(result[i])
+        return s_list
+
+    else:
+        return [s]
 #메인
 f = open('testcase.txt', 'r')
 line = f.readline().rstrip()
-ret = [[len(line),line]]
-for i in range(1,len(line)//2):
-    length, s = RLE(i,ret[-1][1])
-    print(s)
-    ret.append([length,s])
+line_list = [line]
+ans = [len(line)]
+for i in range(len(line)//2,0,-1):
+    new_line = []
+    for line in line_list:
+        ret = choose(i, line)
+        new_line += RLE(ret, line)
+    line_list = new_line
+
+m = len(line_list[0])
+for line in line_list:
+    if len(line) < m:
+        m = len(line)
+
+print(line_list)
+print(m)
+#     ans.append(len(line))
+# ans.sort()
+# print(ans[0])
