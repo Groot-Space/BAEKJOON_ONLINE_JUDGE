5044885b05a65130e641ba2e2ac40d9e719f5184
Falconno7 <xogusxogus39@naver.com>
2022-03-16 23:50:51 +0900

    solve boj 3015

--- 태현/스택의 활용/백준 문제/3015_오아시스 재결합_백준/3015_solution.py
+++ 태현/스택의 활용/백준 문제/3015_오아시스 재결합_백준/3015_solution.py
@@ -1,7 +1,7 @@
 from sys import stdin as f
 from collections import deque
 
-f = open('testcase.txt', 'r')
+f = open('testcase2.txt', 'r')
 
 k = int(f.readline().split()[0])
 line = []
@@ -9,16 +9,34 @@ total_count = 0
 for i in range(k):
     line.append(int(f.readline().split()[0]))
 
-for j in range(len(line)-1):
-    stack = deque()
-    stack.append(line[j])
-    stack.append(line[j+1])
-    total_count += 1
-    for i in range(j+2,len(line)):
-        if line[j] < stack[-1] :
-            break
-        elif stack[-1] < line[i]:
-            stack.append(line[i])
+# print(line)
+stack = deque()
+
+
+for i in range(0,len(line)):
+    if len(stack) == 0:
+        stack.append([line[i], 1])
+        continue
+
+    elif stack[-1][0] == line[i]:
+        stack[-1][1] += 1
+        continue
+
+    while len(stack) != 0 and stack[-1][0] < line[i]:
+        if stack[-1][1] == 1:
+            stack.pop()
             total_count += 1
 
+        else:
+            total_count += stack.pop()[1] - 1
+            total_count += 1
+
+    if len(stack) != 0:
+        stack.append([line[i], 1])
+        total_count += 1
+
+    else :
+        stack.append([line[i],1])
+
 print(total_count)
+
